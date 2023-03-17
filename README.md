69f00a0cd48728942bc9bd75c64f6d8399bdf5a6
stanfrbd <stan@thinkpad>
2023-03-17 15:05:58 +0100

    refactored and added Excel export option

--- README.md
+++ README.md
@@ -9,14 +9,16 @@ pip install -r requirements.txt
 ## Usage
 
 ```
-usage: security_headers_check.py [-h] [-u URL] [-i INPUT_FILE]
+usage: security_headers_check.py [-h] [-u URL] [-i INPUT_FILE] [-f {csv,xlsx}]
 
-optional arguments:
+options:
   -h, --help            show this help message and exit
   -u URL, --url URL     http://example.com -- it will export to csv in the current directory
   -i INPUT_FILE, --input-file INPUT_FILE
                         Choose the path to input file containing URLs e.g. "test.txt" -- it will export to csv in the
                         current directory
+  -f {csv,xlsx}, --format {csv,xlsx}
+                        Choose the export format: csv (default) or xlsx
 ```
 
 ## Proxy
@@ -53,16 +55,12 @@ toto.cuik
 ## Result of execution
 
 ```
-#  duckduckgo.com
-
 Title:  Scan results for duckduckgo.com
 Score:  A
 URL:    https://securityheaders.com/?q=duckduckgo.com&followRedirects=on
 HTTP Forwarding:  No forwarding
 
 Generated CSV: ./security-headers-2022-11-09-16_55_14-export.csv
-
-#  google.com
 
 Title:  Scan results for google.com
 Score:  D
@@ -71,8 +69,6 @@ HTTP Forwarding:  https://www.google.com/
 
 Generated CSV: ./security-headers-2022-11-09-16_55_14-export.csv
 
-#  facebook.com
-
 Title:  Scan results for facebook.com
 Score:  A
 URL:    https://securityheaders.com/?q=facebook.com&followRedirects=on
@@ -80,16 +76,12 @@ HTTP Forwarding:  https://www.facebook.com/
 
 Generated CSV: ./security-headers-2022-11-09-16_55_14-export.csv
 
-#  example.com
-
 Title:  Scan results for example.com
 Score:  F
 URL:    https://securityheaders.com/?q=example.com&followRedirects=on
 HTTP Forwarding:  No forwarding
 
 Generated CSV: ./security-headers-2022-11-09-16_55_14-export.csv
-
-#  toto.cuik
 
 Title:  Scan results for toto.cuik
 Score:  Unknown
@@ -111,3 +103,13 @@ facebook.com,A,https://www.facebook.com/,No issue,Permissions-Policy | Referrer-
 example.com,F,No forwarding,Content-Security-Policy | X-Content-Type-Options | ,X-Frame-Options | Permissions-Policy | Referrer-Policy | ,Warnings - Site is using HTTPThis site was served over HTTP and did not redirect to HTTPS.  - ,https://securityheaders.com/?q=example.com&followRedirects=on
 toto.cuik,Unknown,Unreachable,Unknown,Unknown,Unknown,https://securityheaders.com/?q=toto.cuik&followRedirects=on
 ```
+
+## Using a file containing domains with Excel export
+
+```
+python3 security_headers_check.py -i sample.txt -f xlsx
+```
+
+## Result of execution in Excel
+
+add image here
--- security_headers_check.py
+++ security_headers_check.py
@@ -1,16 +1,18 @@
 #!/usr/bin/env python3
 
-# Stanislas M. 2022-11-09
+# Stanislas M. 2023-03-17
 
 """
-usage: security_headers_check.py [-h] [-u URL] [-i INPUT_FILE]
+usage: security_headers_check_copy.py [-h] [-u URL] [-i INPUT_FILE] [-f {csv,xlsx}]
 
-optional arguments:
+options:
   -h, --help            show this help message and exit
   -u URL, --url URL     http://example.com -- it will export to csv in the current directory
   -i INPUT_FILE, --input-file INPUT_FILE
                         Choose the path to input file containing URLs e.g. "test.txt" -- it will export to csv in the
                         current directory
+  -f {csv,xlsx}, --format {csv,xlsx}
+                        Choose the export format: csv (default) or xlsx
 """
 
 import json
@@ -20,9 +22,11 @@ import requests
 from bs4 import BeautifulSoup
 from pathlib import Path
 from datetime import datetime
-
-# CSV separator
-SEP = ","
+import os
+import sys
+from openpyxl import Workbook
+from openpyxl.styles import PatternFill
+from openpyxl.worksheet.table import Table, TableStyleInfo
 
 # Your proxy here...
 proxy = ""
@@ -66,9 +70,6 @@ REQUIRED_HEADERS_HTTP_10_OR_11 = [ "Cache-Control", "Content-Security-Policy", "
 
 OPTIONAL_HEADERS = [ "Access-Control-Allow-Origin", "Location", "Set-Cookie", "WWW-Authenticate", "X-Frame-Options", "X-XSS-Protection", "Permissions-Policy", "Referrer-Policy" ]
 
-# CSV Headers
-csv = "site" + SEP + "score" + SEP + "httpforwarding" + SEP + "missing_required_headers" + SEP + "missing_optional_headers" + SEP + "warnings" + SEP + "security_headers_url" + "\n"
-
 # CHECKS
 
 # Credit: https://www.adamsmith.haus/python/answers/how-to-remove-empty-lines-from-a-string-in-python
@@ -80,11 +81,69 @@ def remove_empty_lines(txt):
       string_without_empty_lines += line.replace(";", " ") + " - "
     return string_without_empty_lines.replace(",", " ")
 
-def export_to_csv():
-    print("\nGenerated CSV: ./security-headers-" + today + "-export.csv\n")
-    f = open("security-headers-" + today + "-export.csv", "a")
-    f.write(csv)
-    f.close()
+def export(data, file_format):
+    if file_format == "csv":
+        print("\nGenerated CSV: ./security-headers-" + today + "-export.csv\n")
+        with open("security-headers-" + today + "-export.csv", "a") as f:
+            f.write("site,score,httpforwarding,missing_required_headers,missing_optional_headers,warnings,security_headers_url\n")
+            for row in data:
+                f.write(",".join(row) + "\n")
+    elif file_format == "xlsx":
+        export_to_excel(data, "security-headers-" + today + "-export.xlsx")
+
+def export_to_excel(data, filename):
+    wb = Workbook()
+    ws = wb.active
+    ws.append(["site", "score", "httpforwarding", "missing_required_headers", "missing_optional_headers", "warnings", "security_headers_url"])
+
+    # Define conditional formatting colors
+    green_fill = PatternFill(start_color="00FF00", end_color="00FF00", fill_type="solid")
+    light_orange_fill = PatternFill(start_color="FFD966", end_color="FFD966", fill_type="solid")
+    orange_fill = PatternFill(start_color="F4B183", end_color="F4B183", fill_type="solid")
+    red_fill = PatternFill(start_color="FF0000", end_color="FF0000", fill_type="solid")
+    dark_gray_fill = PatternFill(start_color="A6A6A6", end_color="A6A6A6", fill_type="solid")
+
+    # Add data to the worksheet
+    for row in data:
+        ws.append(row)
+
+    # Apply conditional formatting
+    for row in ws.iter_rows(min_row=2):
+        # Score column (column B)
+        if row[1].value.startswith("A"):
+            row[1].fill = green_fill
+        elif row[1].value.startswith("B"):
+            row[1].fill = light_orange_fill
+        elif row[1].value.startswith("C"):
+            row[1].fill = orange_fill
+        elif row[1].value.startswith("D") or row[1].value.startswith("E") or row[1].value.startswith("F"):
+            row[1].fill = red_fill
+        elif row[1].value == "Unknown":
+            row[1].fill = dark_gray_fill
+
+        # httpforwarding column (column C)
+        if "amazon" in row[2].value:
+            row[2].fill = orange_fill
+
+        # missing_required_headers column (column D)
+        if row[3].value == "No issue":
+            row[3].fill = green_fill
+        elif "|" in row[3].value:
+            row[3].fill = red_fill
+
+        # warnings column (column F)
+        if "-" in row[5].value:
+            row[5].fill = orange_fill
+
+    # Add table with filters
+    table = Table(displayName="ResultsTable", ref="A1:G{}".format(ws.max_row))
+    table_style = TableStyleInfo(name="TableStyleMedium9", showFirstColumn=False, showLastColumn=False, showRowStripes=True, showColumnStripes=False)
+    table.tableStyleInfo = table_style
+    ws.add_table(table)
+
+    # Save the workbook
+    wb.save(filename)
+
 
 def check_missing_required_headers(txt):
     missing_found = False
@@ -134,6 +193,8 @@ def check_http_forwarding(url):
 
 def scan(url):
     global csv
+    
+    data = []
 
     proxy_servers = { 'http': proxy, 'https': proxy }
 
@@ -190,37 +251,36 @@ def scan(url):
         print("Score: ", score)
         print("URL:   ", securityheaders_url)
         print("HTTP Forwarding: ", httpforwarding)
-        
-        # CSV text
-        csv += site + SEP + score + SEP + httpforwarding + SEP + missing_req_headers + SEP + missing_opt_headers + SEP + warnings + SEP + securityheaders_url + "\n"
-
-        # Export
-        export_to_csv()
-
-        # clear variables
-        csv = ""
 
     else:
         raise Exception("HTTP error: " + str(base_request.status_code))
+    
+    return [site, score, httpforwarding, missing_req_headers, missing_opt_headers, warnings, securityheaders_url]
 
 # URL -u / --url
 
-def action_url(txt):
+def action_url(txt, export_format):
     print("# ", txt)
-    scan(txt)
+    data = [scan(txt)]
+    export(data, export_format)
 
 # INPUT_FILE -i / --input-file
 
-def action_file(txt):
+def action_file(txt, export_format):
+    all_data = []
+
     if Path(txt).is_file():
         input_file = open(txt, "r")
         data = input_file.readlines()
         input_file.close()
         if data != []:
             for i in range (0, len(data)):
-                action_url(data[i].rstrip('\n'))
+                output_data = scan(data[i].rstrip('\n'))
+                all_data.append(output_data)    
     else:
         print("\"" + txt + "\" is not a valid file, aborting.")
+    
+    export(all_data, export_format)
 
 # MAIN
 
@@ -228,15 +288,16 @@ def main():
     parser = argparse.ArgumentParser()
     parser.add_argument('-u','--url', help='http://example.com -- it will export to csv in the current directory')
     parser.add_argument('-i','--input-file', help='Choose the path to input file containing URLs e.g. "test.txt" -- it will export to csv in the current directory')
+    parser.add_argument('-f', '--format', choices=['csv', 'xlsx'], default='csv', help='Choose the export format: csv (default) or xlsx')
     
     global args
     args = parser.parse_args()
 
     if args.url:
-        action_url(args.url)
+        action_url(args.url, args.format)
 
     if args.input_file:
-        action_file(args.input_file)    
+        action_file(args.input_file, args.format)     
 
 if __name__ == "__main__":
     try: 
